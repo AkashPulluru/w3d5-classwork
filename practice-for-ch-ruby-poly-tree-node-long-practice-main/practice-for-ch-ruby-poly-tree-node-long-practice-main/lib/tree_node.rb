2744c3683a6a38309bc651628977c16adc73f15f
elevenstand7 <elevenstand7@gmail.com>
2023-07-14 15:31:31 -0400

    finished

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,57 +1,72 @@
 class PolyTreeNode
     def initialize(value)
-    @value = value 
-    @parent = nil 
-    @children = [] 
-    end 
+    @value = value
+    @parent = nil
+    @children = []
+    end
 
     def parent
         @parent
-    end 
+    end
 
     def children
         @children
-    end 
+    end
 
-    def value 
+    def value
         @value
-    end 
+    end
 
     def parent=(node)
-        if self.parent    
-            self.parent.children.delete(self) 
-        end 
-            @parent = node 
+        if self.parent
+            self.parent.children.delete(self)
+        end
+            @parent = node
             self.parent.children << self unless self.parent.nil?
-        self 
-    end 
+        self
+    end
 
     def add_child(child_node)
         child_node.parent=(self)
-    end 
+    end
 
     def remove_child(child_node)
-        raise "No Parent" if !child_node.parent 
+        raise "No Parent" if !child_node.parent
         child_node.parent=(nil)
-    end 
+    end
 
     def dfs(target_value)
-    if self.value == target_value 
-        return self 
-    end 
+    if self.value == target_value
+        return self
+    end
 
-    i = 0 
-    while i < self.children.length 
+    i = 0
+    while i < self.children.length
         current = self.children[i]
         result = current.dfs(target_value)
         if result
-            return result 
-        end 
-        i += 1 
-    end 
-    return nil 
-    end 
+            return result
+        end
+        i += 1
+    end
+    return nil
+    end
 
+    def bfs(target_value)
+        array = []
+        array << self
+
+        while array.length > 0
+            curr = array.shift
+            if curr.value == target_value
+                return curr
+            else
+                array += curr.children
+            end
+        end
+        nil
+
+    end
 
 
 end
