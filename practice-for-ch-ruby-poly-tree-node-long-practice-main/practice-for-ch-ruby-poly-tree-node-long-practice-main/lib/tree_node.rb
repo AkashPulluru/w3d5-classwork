161cabad230c348be5601e5a71fe790fdcd4b2e8
AkashPulluru <akashpulluru@gmail.com>
2023-07-14 13:55:22 -0400

    second commit

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,29 @@
 class PolyTreeNode
+    def initialize(value)
+    @value = value 
+    @parent = nil 
+    @children = [] 
+    end 
+
+    def parent
+        @parent
+    end 
+
+    def children
+        @children
+    end 
+
+    def value 
+        @value
+    end 
+
+    def parent=(node)
+        if !self.parent.nil?      
+            @parent = node 
+            if !node.children.include?(self)
+            node.children << self
+            end
+        end 
+    end 
   
 end
