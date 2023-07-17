7f2781ce157b94602d5f20e005f0900a061cbf91
AkashPulluru <akashpulluru@gmail.com>
2023-07-17 10:49:12 -0400

    starting commit w4d1

--- practice-for-ch-ruby-knights-travails-long-practice-main/practice-for-ch-ruby-knights-travails-long-practice-main/Knights_Travails_part_1.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/practice-for-ch-ruby-knights-travails-long-practice-main/Knights_Travails_part_1.rb
@@ -76,14 +76,53 @@ end
 class KnightPathFinder
     def initialize(starting_position)
     self.root_node = starting_position 
+    @considered_positions = starting_position 
+    @current_position = starting position 
     end 
 
     # def build_move_tree 
     
     # end
 
-    def valid_moves 
-    
+    def self.valid_moves 
+        x_coordinate = @current_position[0]
+        y_coordinate = @current_position[1]
+        holder_array = []
+        potential_moves = [] 
+
+        (x_coordinate-1..x_coordinate+1).each do |x_coordinates|
+            (y_coordinate-1..y_coordinate+1).each do |y_coordinates|
+                if [x_coordinates, y_coordinates] != @current_position
+                    holder_array << [x_coordinates, y_coordinates]
+                end 
+            end 
+        end 
+
+        holder_array.each do |coordinate_pairs|
+            if coordinate_pairs[0] >= 0 & coordinate_pairs[1] >= 0 
+                potential_moves << coordinate_pairs  
+            end 
+        end 
+
+        potential_moves.each do |moves|
+            if !@considered_positions.include?(moves)
+                @considered_positions << moves 
+            end 
+        end 
+
+    end 
+
+    def new_move_positions 
+    potential_positions = [] 
+
+    self.valid_moves.each do |move|
+        if !considered_positions.include?(move)
+            potential_positions << potential_positions 
+        end 
+    end 
+
+    return potential_positions 
+
     end 
     
 end 
